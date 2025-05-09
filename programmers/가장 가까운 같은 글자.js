21d8b542239c1b736c0b135cac72c0aa375a714e
JY <81917385+jacarandaring@users.noreply.github.com>
2025-05-10 02:34:31 +0900

    [programmers] 2번째 시도 (fail)

--- programmers/가장 가까운 같은 글자.js
+++ programmers/가장 가까운 같은 글자.js
@@ -8,8 +8,9 @@ function solution(s) {
     for (let i = 0; i < s.split('').length; i++) {
         let indexs = s.split('').slice(0, i).map((t, idx) => {
             if (t === s[i]) return idx;
-        }).filter((idx) => idx);
-        const diff = indexs.length > 0 ? i - indexs[indexs.length - 1] : -1;
+        });
+        const target = indexs.filter((index) => index !== undefined);
+        const diff = target.length > 0 ? i - target[target.length - 1] : -1;
         answer.push(diff);
     }
     return answer;
