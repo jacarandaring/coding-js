b3da7d291851048856664c7c88895d861c25c04b
j <jacarandaring@gmail.com>
2025-05-11 16:23:39 +0900

    [programmers] 둘만의 암호 - 두 번째 시도

--- programmers/둘만의 암호.js
+++ programmers/둘만의 암호.js
@@ -1,25 +1,40 @@
 /**
  * [프로그래머스] 둘만의 암호
  * url: https://school.programmers.co.kr/learn/courses/30/lessons/155652
- * 작성일: 2025-03-31
+ * 작성일: 2025-04-16
  */
 function solution(s, skip, index) {
-    var answer = '';
-    const aCode = 'a'.charCodeAt();
-    const zCode = 'z'.charCodeAt();
-    const skipList = skip.split('').map((s) => s.charCodeAt());
+    const start = 'a'.charCodeAt(); // 97
+    const end = 'z'.charCodeAt(); // 122
+    const sp = skip.split('').map((sp) => sp.charCodeAt()).sort((a, b) => a - b);
     
-    s.split('').forEach((ss) => {
-        let target = ss.charCodeAt() + index;
-        let [start, end] = [ss.charCodeAt(), target];
-        const toSkip = skipList.filter((sl) => sl >= start && sl <= end);
-        if (toSkip) {
-            target += toSkip.length;
+    let ss = s.split('').map((ss) => {
+        let result = ss.charCodeAt() + index;
+        
+        // index만큼 뒤의 알파벳으로 바꾸면서 skip 체크
+        let skips = sp.filter((skip) => ss.charCodeAt() <= skip && skip <= result).length;
+        if (result > end) {
+            skips = sp.filter((skip) => (start <= skip && skip <= result - end + start - 1) || (ss.charCodeAt() <= skip && skip <= end)).length;
         }
-        if (target > zCode) {
-            target -= (zCode - aCode + 1); // return to a
+        result += skips;
+        result = result > end ? result - end + start - 1 : result;
+        
+        // 건너뛴 후 skip 체크
+        console.log(result)
+        const skipping = sp.findIndex((skip) => skip === result);
+        console.log(skipping)
+        if (skipping > -1) {
+            for (let i = skipping; i < sp.length; i++) {
+                if (sp[i] === result) {
+                    result++;
+                    result = result > end ? result - end + start - 1 : result;
+                }
+                else break;
+            }
         }
-        answer += String.fromCharCode(target);
+
+        return result;
     });
-    return answer;
+    
+    return ss.map((s) => String.fromCharCode(s)).join('');
 }
