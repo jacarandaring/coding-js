ff4ea390bcd14bc2ec4a1ed36a40174b57bbbb26
JY <81917385+jacarandaring@users.noreply.github.com>
2025-07-21 21:14:46 +0900

    [programmers] *정규표현식* - *지수표기법*으로 인식하는 경우 제외
    - 정규표현식 ’또는‘ 조건은 ‘|’ 문자로 표현

--- programmers/문자 다루기 기본.js
+++ programmers/문자 다루기 기본.js
@@ -4,6 +4,6 @@
  * 작성일: 2025-07-21
  */
 function solution(s) {
-    // 지수표기법으로 인식하는 경우 제외
-    return !s.includes('e') && (s.trim().length === 4 || s.trim().length === 6) && !isNaN(Number(s));
+    const regex = new RegExp(/^\d{4}$|^\d{6}$/);
+    return regex.test(s);
 }
