793e3909d25f556941641ec8bd54ee62d9a2133b
JY <81917385+jacarandaring@users.noreply.github.com>
2025-07-21 20:18:58 +0900

    [programmers] 문제 해결

---
+++ programmers/같은 숫자는 싫어.js
@@ -0,0 +1,17 @@
+/**
+ * [프로그래머스] 같은 숫자는 싫어
+ * url: https://school.programmers.co.kr/learn/courses/30/lessons/12906
+ * 작성일: 2025-07-21
+ */
+function solution(arr)
+{
+    const result = [];
+    let dup = -1;
+    arr.forEach((a) => {
+        if (dup !== a) {
+            result.push(a);
+            dup = a;
+        }
+    });
+    return result;
+}
