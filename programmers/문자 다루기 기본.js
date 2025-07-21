a972baa99b489d084df738c43241f8e6964226a0
JY <81917385+jacarandaring@users.noreply.github.com>
2025-07-21 21:00:42 +0900

    [programmers] *지수표기법*으로 인식하는 경우 제외

--- programmers/문자 다루기 기본.js
+++ programmers/문자 다루기 기본.js
@@ -4,5 +4,6 @@
  * 작성일: 2025-07-21
  */
 function solution(s) {
-    return (s.trim().length === 4 || s.trim().length === 6) && !isNaN(Number(s));
+    // 지수표기법으로 인식하는 경우 제외
+    return !s.includes('e') && (s.trim().length === 4 || s.trim().length === 6) && !isNaN(Number(s));
 }
